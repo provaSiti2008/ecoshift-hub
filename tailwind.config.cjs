db2813a3aa0fa423b19134465d001ba6996eb12a
provaSiti2008 <provasito483@gmail.com>
2026-02-13 00:18:36 +0100

    ciao2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
     content: [
         "./index.html",
+        "./App.tsx",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./index.tsx",
     ],
     theme: {
         extend: {
