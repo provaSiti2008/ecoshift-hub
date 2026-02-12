617aeb59c4de899fe15bdae0341d74b41e2f00ea
provaSiti2008 <provasito483@gmail.com>
2026-02-13 00:15:37 +0100

    ciao

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+    content: [
+        "./index.html",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    50: '#f5f7ff',
+                    100: '#ebf0fe',
+                    200: '#ced9fd',
+                    300: '#b1c2fb',
+                    400: '#7695f8',
+                    500: '#3b67f5',
+                    600: '#355ddd',
+                    700: '#2c4eb8',
+                    800: '#233e93',
+                    900: '#1d3378',
+                },
+            },
+            borderRadius: {
+                '2xl': '1rem',
+                '3xl': '1.5rem',
+                '4xl': '2rem',
+            }
+        },
+    },
+    plugins: [],
+}
